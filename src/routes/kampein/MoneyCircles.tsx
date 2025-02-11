f415e0ea5ba5f7de37ee58dc8746b0e0b5fc3ef3
TamamTamar <tamar.tamam770@gmail.com>
2025-02-11 15:03:50 +0200

    fix

--- src/routes/kampein/MoneyCircles.tsx
+++ src/routes/kampein/MoneyCircles.tsx
@@ -21,14 +21,15 @@ const MoneyCircles = () => {
                 <p className="amount">₪{amount}</p>
                 <div className="to-month">לחודש</div>
             </div>
-
         </button>
     );
 
     return (
         <div className="money-circles-container">
             <div className="money-circles">
-                <button className="circle" onClick={() => handleButtonClick(0)}><span className='another-amount'>סכום  <br /> חופשי</span></button>
+                <button className="circle" onClick={() => handleButtonClick(0)}>
+                    <span className='another-amount'>סכום  <br /> חופשי</span>
+                </button>
                 {renderButton(300)}
                 {renderButton(500)}
                 {renderButton(1000)}
@@ -38,7 +39,9 @@ const MoneyCircles = () => {
             {showPaymentForm && (
                 <div className="overlay">
                     <div className="payment-form-wrapper">
-                        <button className="close-button" onClick={closePaymentForm}><img src="/img/kampein/x.svg" alt="Close" /></button>
+                        <button className="close-button" onClick={closePaymentForm}>
+                            <img src="/img/kampein/x.svg" alt="Close" />
+                        </button>
                         <PaymentForm monthlyAmount={amount} />
                     </div>
                 </div>
