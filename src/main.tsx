c33f4834925baf826da24f76da935988e72e2895
TamamTamar <tamar.tamam770@gmail.com>
2025-02-12 09:45:41 +0200

    main

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,19 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router.tsx";
+import { AuthContextProvider } from "./contexts/AuthContext.tsx";
+import { FilterProvider } from "./contexts/FilterContext.tsx";
+import { SearchProvider } from "./contexts/SearchContext.tsx";
+import { AlertProvider } from "./contexts/AlertContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-
-
-  <RouterProvider router={router} />
-
+  <AuthContextProvider>
+    <FilterProvider>
+      <SearchProvider>
+        <AlertProvider>
+          <RouterProvider router={router} />
+        </AlertProvider>
+      </SearchProvider>
+    </FilterProvider>
+  </AuthContextProvider>
 );
