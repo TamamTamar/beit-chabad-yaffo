173f2651582d6da929a9719e1bdc2ef1d64d5017
TamamTamar <tamar.tamam770@gmail.com>
2025-02-16 10:16:04 +0200

    fix

--- src/routes/kampein/PaymentForm.tsx
+++ src/routes/kampein/PaymentForm.tsx
@@ -61,7 +61,7 @@ const PaymentForm = ({ monthlyAmount }) => {
             Currency: 1,
             Groupe: data.Groupe || "",
             Comment: data.Comment || "",
-            CallBack: "https://node-beit-chabad-yaffo.onrender.com/api/nedarim",
+            CallBack: "https://node-beit-chabad-yaffo.onrender.com/api/payment/nedarim",
             CallBackMailError: "lchabadyaffo@gmail.com",
         };
 
