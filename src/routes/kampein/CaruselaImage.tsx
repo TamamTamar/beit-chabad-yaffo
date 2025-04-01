9adf3e52ca38cd20a1cb8be09ae51cafe0eb5069
TamamTamar <tamar.tamam770@gmail.com>
2025-04-01 10:11:09 +0300

    df

--- src/routes/kampein/CaruselaImage.tsx
+++ src/routes/kampein/CaruselaImage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import './carousel.scss';
 
 const CaruselaImage = () => {
@@ -59,28 +59,20 @@ const CaruselaImage = () => {
         return () => clearInterval(interval); // לנקות את ה-interval כשמעבר לדף אחר
     }, [images.length]);
 
-    // הצגת התמונות הנוכחיות (3 תמונות)
-    const displayedImages = [
-        images[(currentIndex) % images.length],
-        images[(currentIndex + 1) % images.length],
-        images[(currentIndex + 2) % images.length],
-    ];
     return (
         <div className="carousel-container">
-            <div className="carousel">
-                <div className="carousel-images">
-                    {displayedImages.map((image, index) => (
-                        <img
-                            className="carousel-img"
-                            key={index}
-                            src={image}
-                            alt={`carousel-img-${index}`}
-                        />
-                    ))}
-                </div>
+            <div className={`carousel translate-${currentIndex}`}>
+                {images.map((image, index) => (
+                    <img
+                        className="carousel-img"
+                        key={index}
+                        src={image}
+                        alt={`carousel-img-${index}`}
+                    />
+                ))}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default CaruselaImage
+export default CaruselaImage;
