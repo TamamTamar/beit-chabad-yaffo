e1082395f44c9acce0dbb83bf162ccde62d9f3ff
TamamTamar <tamar.tamam770@gmail.com>
2025-04-02 12:54:43 +0300

    image

--- src/routes/kampein/CaruselaImage.tsx
+++ src/routes/kampein/CaruselaImage.tsx
@@ -13,6 +13,40 @@ const CaruselaImage = () => {
         '/img/8.jpeg',
         '/img/9.jpeg',
         '/img/10.jpeg',
+        '/img/11.jpeg',
+        '/img/12.jpeg',
+        '/img/13.jpeg',
+        '/img/14.jpeg',
+        '/img/15.jpeg',
+        '/img/16.jpeg',
+        '/img/17.jpeg',
+        '/img/18.jpeg',
+        '/img/19.jpeg',
+        '/img/20.jpeg',
+        '/img/21.jpeg',
+        '/img/22.jpeg',
+        '/img/23.jpeg',
+        '/img/24.jpeg',
+        '/img/25.jpeg',
+        '/img/26.jpeg',
+        '/img/27.jpeg',
+        '/img/28.jpeg',
+        '/img/29.jpeg',
+        '/img/30.jpeg',
+        '/img/31.jpeg',
+        '/img/32.jpeg',
+        '/img/33.jpeg',
+        '/img/34.jpeg',
+        '/img/35.jpeg',
+        '/img/36.jpeg',
+        '/img/37.jpeg',
+        '/img/38.jpeg',
+        '/img/39.jpeg',
+        '/img/40.jpeg',
+        '/img/41.jpeg',
+        '/img/42.jpeg',        
+        '/img/43.jpeg',
+        '/img/44.jpeg',
     ];
 
     const [currentIndex, setCurrentIndex] = useState(0);
@@ -29,7 +63,7 @@ const CaruselaImage = () => {
     const nextSlide = () => {
         setIsAnimating(true); // הפעלת האנימציה
         setTimeout(() => setIsAnimating(false), 500); // כיבוי האנימציה לאחר 500ms
-        setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+        setCurrentIndex((prevIndex) => (prevIndex + 3) % images.length);
     };
 
     const getDisplayedImages = () => {
