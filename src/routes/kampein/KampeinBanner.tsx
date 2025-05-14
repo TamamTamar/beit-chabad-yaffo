d5c30ad0172e386196815766c9ad79083dcf9025
TamamTamar <tamar.tamam770@gmail.com>
2025-05-14 13:01:04 +0300

    kampein banner

--- src/routes/kampein/KampeinBanner.tsx
+++ src/routes/kampein/KampeinBanner.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import './KampeinBanner.scss';
 import Aboutkampein from './AboutKampein';
 
-
 const KampeinBanner = () => {
   const [showInfo, setShowInfo] = useState(false);
 
@@ -16,11 +15,30 @@ const KampeinBanner = () => {
 
   return (
     <div className="kampein-banner">
-      <button className='kampein-banner-button' onClick={handleButtonClick}>אודות הקמפיין</button>
+      <img src="/img/kampein/logo chabad.svg" alt="logo" className="kampein-banner-image" />
+      <h2 className="kampein-banner-title">אור ליהודי יפו – בזכותכם!</h2>
+      <p className="kampein-banner-description">הצטרפו לתמיכה בפעילות השוטפת של בית חב״ד יפו</p>
+
+      <div className="kampein-buttons">
+        <button className="kampein-banner-button" onClick={handleButtonClick}>
+          אודות הקמפיין
+        </button>
+        <a
+          href="https://www.matara.pro/nedarimplus/online/?mosad=7004217"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="donate-button"
+        >
+          לתרומה
+        </a>
+      </div>
+
       {showInfo && (
         <div className="overlay">
           <div className="info-wrapper">
-            <button className="close-button" onClick={closeInfo}><img src="/img/kampein/x.svg" alt="Close" /></button>
+            <button className="close-button" onClick={closeInfo}>
+              <img src="/img/kampein/x.svg" alt="Close" />
+            </button>
             <div className="info-content">
               <Aboutkampein />
             </div>
