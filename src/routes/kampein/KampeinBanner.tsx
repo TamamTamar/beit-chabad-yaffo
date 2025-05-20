6e12241a35a4b6a6988270725a546563ec8692bd
TamamTamar <tamar.tamam770@gmail.com>
2025-05-20 10:08:51 +0300

    bn

--- src/routes/kampein/KampeinBanner.tsx
+++ src/routes/kampein/KampeinBanner.tsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import './KampeinBanner.scss';
 import Aboutkampein from './AboutKampein';
-import PaymentForm from './PaymentForm'; // ייבוא רכיב הטופס
+import PaymentForm from './PaymentForm';
 import { useNavigate } from 'react-router-dom';
 
 const KampeinBanner = () => {
   const [showInfo, setShowInfo] = useState(false);
-  const [showPaymentForm, setShowPaymentForm] = useState(false); // state לניהול תצוגת הטופס
+  const [showPaymentForm, setShowPaymentForm] = useState(false);
   const navigate = useNavigate();
 
   const handleButtonClick = () => {
@@ -17,17 +17,15 @@ const KampeinBanner = () => {
     setShowInfo(false);
   };
 
-  if (showPaymentForm) {
-    return <PaymentForm monthlyAmount={0} />; // הצגת רכיב הטופס עם סכום ברירת מחדל
-  }
+  const closePaymentForm = () => {
+    setShowPaymentForm(false);
+  };
 
   return (
     <div className="kampein-banner">
-
       <h2 className="kampein-banner-title">אור ליהודי יפו – בזכותכם!</h2>
       <p className="kampein-banner-description">הצטרפו לתמיכה בפעילות השוטפת של בית חב״ד יפו</p>
       <div className="kampein-buttons">
-
         <button
           className="donate-button"
           onClick={() => setShowPaymentForm(true)}
@@ -40,9 +38,6 @@ const KampeinBanner = () => {
         >
           אודות הקמפיין
         </button>
-        <button className="info-button" onClick={handleButtonClick}>
-          אודות הקמפיין
-        </button>
       </div>
 
       {showInfo && (
@@ -53,6 +48,19 @@ const KampeinBanner = () => {
             </button>
             <div className="info-content">
               <Aboutkampein />
+            </div>
+          </div>
+        </div>
+      )}
+
+      {showPaymentForm && (
+        <div className="overlay">
+          <div className="info-wrapper">
+            <button className="close-button" onClick={closePaymentForm}>
+              <img src="/img/kampein/x.svg" alt="Close" />
+            </button>
+            <div className="info-content">
+              <PaymentForm monthlyAmount={0} />
             </div>
           </div>
         </div>
