74269f3ded27ae342d955f39e49e7680fda5325b
TamamTamar <tamar.tamam770@gmail.com>
2025-06-05 10:28:50 +0300

    nm

--- src/services/donation-service.ts
+++ src/services/donation-service.ts
@@ -1,9 +1,9 @@
 import { Donation } from "../@Types/chabadType";
 
-const baseUrl = "https://node-beit-chabad-yaffo.onrender.com/api"; // כתובת הבסיס של ה-API שלך
+const baseUrl = "https://node-beit-chabad-yaffo.onrender.com/api/payment"; // כתובת הבסיס של ה-API שלך
 
 export const getAllDonations = async (): Promise<Donation[]> => {
-  const response = await fetch(`${baseUrl}/donations`);
+  const response = await fetch(`${baseUrl}/nedarim/payments`);
   
   if (!response.ok) {
     throw new Error('Failed to fetch donations');
