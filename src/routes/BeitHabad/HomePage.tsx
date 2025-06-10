dbc5fe8115c876aff3a3c93cd70ee7e0a0f0bfe4
TamamTamar <tamar.tamam770@gmail.com>
2025-06-10 09:32:24 +0300

    j

--- src/routes/BeitHabad/HomePage.tsx
+++ src/routes/BeitHabad/HomePage.tsx
@@ -1,14 +1,15 @@
 import ChabadAboutSection from "../../components/aboutchabad/AboutChabad"
 import CandleLightingTimes from "../../components/CandleLightingTimes/CandleLightingTimes"
 import MainBanner from "../../components/mainBanner/MainBanner"
-import BranchesSection from "../BranchesSectio"
-import KosherServicesStrip from "../KosherServicesStrip"
+import BranchesSection from "./BranchesSection"
 import ContactFooter from "./ContactFooter"
 import DonationPreviewSection from "./DonationPreviewSection"
 import GallerySection from "./GallerySection"
 import JoinCommunityBanner from "./JoinCommunityBanner"
+import KosherServicesStrip from "./KosherServicesStrip"
 import ShabatForm from "./ShabatForm"
 import WhatsAppFloatingButton from "./WhatsAppFloatingButton"
+
 
 const HomePage = () => {
   return (
