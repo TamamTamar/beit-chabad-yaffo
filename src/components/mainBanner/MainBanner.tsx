e4a33949263cb0378b178c822562d10150ad8008
TamamTamar <tamar.tamam770@gmail.com>
2025-06-10 13:15:40 +0300

    main

--- src/components/mainBanner/MainBanner.tsx
+++ src/components/mainBanner/MainBanner.tsx
@@ -4,8 +4,8 @@ const MainBanner = () => {
   return (
     <section className="main-banner">
       <div className="banner-content">
-        <h1>בית חב״ד יפו</h1>
-        <h2>ברוכים הבאים ליפו</h2>
+        <h1 className='banner-title'>בית חב״ד יפו</h1>
+        <h2 className='banner-subtitle'>ברוכים הבאים ליפו</h2>
         <div className="banner-buttons">
           <button className="info-btn">מידע</button>
           <button className="shabbat-btn">שבת וחג</button>
