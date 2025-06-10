deefab457d377a4aa4b155b64c07f1319ca2efe6
TamamTamar <tamar.tamam770@gmail.com>
2025-06-10 10:44:07 +0300

    nav

--- src/routes/BeitHabad/NavBarChabad.tsx
+++ src/routes/BeitHabad/NavBarChabad.tsx
@@ -1,42 +1,48 @@
-
 import { NavLink, useLocation } from 'react-router-dom';
 import { useEffect } from 'react';
 import './NavBarChabad.scss';
 
 const scrollToSection = (id: string) => {
-    const el = document.getElementById(id);
-    if (el) el.scrollIntoView({ behavior: 'smooth' });
+  const el = document.getElementById(id);
+  if (el) el.scrollIntoView({ behavior: 'smooth' });
 };
 
 const NavBarChabad = () => {
-    const location = useLocation();
+  const location = useLocation();
 
-    useEffect(() => {
-        if (location.hash) {
-            const anchor = location.hash.replace('#', '');
-            scrollToSection(anchor);
-        }
-    }, [location]);
+  useEffect(() => {
+    if (location.hash) {
+      const anchor = location.hash.replace('#', '');
+      scrollToSection(anchor);
+    }
+  }, [location]);
 
-    return (
-        <header className="navbar">
-            <div className="navbar__container">
-                <NavLink to="/" className="navbar__logo">
-                    <img src="img/logo-nav.png" alt="בית חב״ד יפו" />
-                </NavLink>
+  return (
+    <header className="navbar">
+      <div className="navbar__container">
+        {/* חלק 1: לוגו */}
+        <div className="navbar__logo">
+          <NavLink to="/">
+            <img src="img/logo-nav.png" alt="בית חב״ד יפו" />
+          </NavLink>
+        </div>
 
-                <nav className="navbar__menu">
-                    <NavLink to="/">דף ראשי</NavLink>
-                    <NavLink to="/beit-chabad">בית חב״ד</NavLink>
-                    <NavLink to="/branches">סניפים</NavLink>
-                    <NavLink to="/shabbat">שבת וחג</NavLink>
-                    <NavLink to="/#shabbat-section">סעודות שבת</NavLink>
-                    <NavLink to="/#zmanim-section">זמני היום</NavLink>
-                    <NavLink to="/donation" className="donation-link">תרומה</NavLink>
-                </nav>
-            </div>
-        </header>
-    );
+        {/* חלק 2: תפריט */}
+        <nav className="navbar__menu">
+          <NavLink to="/">דף ראשי</NavLink>
+          <NavLink to="/beit-chabad">בית חב״ד</NavLink>
+          <NavLink to="/branches">סניפים</NavLink>
+          <NavLink to="/#zmanim-section">זמני היום</NavLink>
+        </nav>
+
+        {/* חלק 3: כפתורים */}
+        <div className="navbar__buttons">
+          <NavLink to="/donation" className="button donate">תרומה</NavLink>
+          <NavLink to="/shabbat" className="button shabbat">שבת וחג</NavLink>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default NavBarChabad;
