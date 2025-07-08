f16dd32a9bc1d5c50b547c0da5aa7545477ff933
TamamTamar <tamar.tamam770@gmail.com>
2025-07-08 09:28:50 +0300

    zx

--- src/services/donation-service.ts
+++ src/services/donation-service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Donation, RefSummary } from "../@Types/chabadType";
 
-const baseUrl = "https://node-beit-chabad-yaffo.onrender.com/api/payment"; // כתובת הבסיס של ה-API שלך
+const baseUrl = "https://node-beit-chabad-yaffo-production.up.railway.app/api/payment"; // כתובת הבסיס של ה-API שלך
 
 export const getAllDonations = async (): Promise<Donation[]> => {
   const response = await axios.get(`${baseUrl}/nedarim/payments`);
