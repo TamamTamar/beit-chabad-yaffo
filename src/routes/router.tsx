b0733a4575d30dceb101bfa24f665b967b2bf6c0
TamamTamar <tamar.tamam770@gmail.com>
2025-08-31 10:10:07 +0300

    bn

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,26 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../layout/BeitChabadLayout";
-import AdminShabbat from "./admin/AdminShabbat";
-
 import CreateNewParasha from "./createParasha/createNewParasha";
 import EditParasha from "./editParasha/EditParasha";
-
 import ParashaDetail from "./BeitHabad/ParashaDetail";
 import ParashaList from "./BeitHabad/ParashaList";
 import ShabatForm from "./BeitHabad/ShabatForm";
-
-import Error from "./Error";
-import AboutCampein from "./Campein/AboutCampein";
-import CampeinPage from "./Campein/CampeinPage";
+import AdminDonation from "./admin/AdminDonation";
 import ChabadDashboard from "./admin/ChabadDashboard";
 import AskRabbiPage from "./AskRabbiPage";
-import AdminDonation from "./admin/AdminDonation";
-import ProtectedRouteAdmin from "./ProtectedRouteAdmin";
-import HomePage from "./BeitHabad/HomePage";
-import BranchesSection from "./BeitHabad/BranchesSection";
 import BeitChabadPage from "./BeitHabad/BeitChabadPage";
-import UnderConstruction from "./UnderConstruction";
-import Confirmation from "./Campein/Confirmation";
+import BranchesSection from "./BeitHabad/BranchesSection";
+import AboutCampein from "./Campein/AboutCampein";
+import CampeinPage from "./Campein/CampeinPage";
+import Error from "./Error";
+import ProtectedRouteAdmin from "./ProtectedRouteAdmin";
 
 
 
